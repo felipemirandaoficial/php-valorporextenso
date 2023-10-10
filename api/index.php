8e6e1c1b74f72c5f3055400d0174c3e349569534
Felipe Miranda <143961165+felipemirandaoficial@users.noreply.github.com>
2023-10-10 19:59:35 -0400

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *"); // Permitir solicitações de qualquer origem
+
+// Outras configurações CORS
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 function traduzirMes($mes) {
   $meses = array(
     'January' => 'Janeiro',
